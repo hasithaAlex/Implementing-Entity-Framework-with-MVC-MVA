c45809befff374a8b5bbd20b4d73a6071629bb19
hasitha <hasithamadusanka90@gmail.com>
2017-08-12 04:56:25 +0530

    Add project files.

---
+++ ConsoleApplication01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MusixContext()) {
+
+                var count = context.Albams.Count();
+                Console.WriteLine(count);
+
+                context.Albams.Add(new Albam { Price =10, Title="Looks Grate"});
+                context.SaveChanges();
+
+                count = context.Albams.Count();
+                Console.WriteLine(count);
+
+                var albams = context.Albams
+                    .Where(o=>o.Price ==10).ToList();
+                Console.WriteLine(albams.Count());
+
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Albam {
+        public int AlbamId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MusixContext : DbContext {
+        public DbSet<Albam> Albams { get; set; }
+    } 
+}
