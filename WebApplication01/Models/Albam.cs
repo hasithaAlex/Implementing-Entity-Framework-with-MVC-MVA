bd59f2c1342f63f19cae89c95d665de8cb7cbb34
hasitha <hasithamadusanka90@gmail.com>
2017-08-12 07:18:41 +0530

    Implementing Entity Framework with MVC - Part 1_6

---
+++ WebApplication01/Models/Albam.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication01.Models
+{
+    public class Albam
+    {
+        public int AlbamId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MusixContext : DbContext
+    {
+        public MusixContext()
+        {
+            Database.Log = s => Debug.WriteLine(s);
+        }
+        public DbSet<Albam> Albams { get; set; }
+    }
+}
