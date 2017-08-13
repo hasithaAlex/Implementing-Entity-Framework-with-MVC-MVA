f15528c7665d820cf4a7e2e5f1849cf97b232d0d
hasitha <hasithamadusanka90@gmail.com>
2017-08-13 12:00:27 +0530

    Start MVA tute

---
+++ 02_MVA_Ex/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_02_MVA_Ex.MvcApplication" Language="C#" %>
---
+++ 02_MVA_Ex/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_02_MVA_Ex.Startup))]
+namespace _02_MVA_Ex
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
